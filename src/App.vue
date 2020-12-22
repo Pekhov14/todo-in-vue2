ca19dc6a04de7e54bddf8079d7a295ba74a3dae0
pehov <pehovanton21@gmail.com>
2020-12-22 22:43:49 +0200

    min todo

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <h1>Todo application</h1>
+    <AddTodo
+      @add-todo="AddTodo"
+    />
+    <hr>
+    <TodoList
+        v-bind:todos="todos"
+        @remove-todo="removeTodo"
+    />
+  </div>
+</template>
+
+<script>
+import TodoList from "@/components/TodoList";
+import AddTodo from "@/components/AddTodo";
+export default {
+  name: 'App',
+  data() {
+    return {
+      todos: [
+        {id: 1, title: 'Хлеб', completed: false},
+        {id: 2, title: 'Молоко', completed: false},
+        {id: 3, title: 'Сыр', completed: false},
+      ]
+    }
+  },
+  methods: {
+    removeTodo(id) {
+      this.todos = this.todos.filter(t => t.id !== id)
+    },
+    AddTodo(todo) {
+      this.todos.push(todo)
+    }
+  },
+  components: {
+    TodoList,
+    AddTodo
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
